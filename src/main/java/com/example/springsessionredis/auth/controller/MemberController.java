9f1e457fba4b8085e65e21304a03c7a20c13442a
pjh5365 <pjh5365@naver.com>
2025-01-17 15:04:45 +0900

    요청형식 수정

--- src/main/java/com/example/springsessionredis/auth/controller/MemberController.java
+++ src/main/java/com/example/springsessionredis/auth/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.example.springsessionredis.auth.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,7 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping("/join")
-	public String join(@RequestBody MemberRequest request) {
+	public String join(MemberRequest request) {
 		memberService.join(request);
 
 		return "회원가입 성공";

fb4f0f9887ee05c4615573981c1ffecccc6d4856
pjh5365 <pjh5365@naver.com>
2025-01-17 16:18:36 +0900

    cors 수정

--- src/main/java/com/example/springsessionredis/auth/config/CorsConfig.java
+++ src/main/java/com/example/springsessionredis/auth/config/CorsConfig.java
@@ -9,8 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
+		// 모든 경로에 대해 CORS 허용
 		registry.addMapping("/**")
-				.allowedOrigins("http://10.0.0.6", "http://223.130.137.86")
-				.allowedMethods("GET", "POST", "PUT", "DELETE");
+				.allowedOrigins("http://10.0.0.6", "http://223.130.137.86")  // 허용할 Origin을 나열
+				.allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+				.allowedHeaders("*")  // 허용할 헤더
+				.allowCredentials(true)  // 쿠키 전송 허용
+				.maxAge(3600);  // CORS 응답 캐시 시간 (1시간)
 	}
 }

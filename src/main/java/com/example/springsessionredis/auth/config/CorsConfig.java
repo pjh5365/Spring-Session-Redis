bd054da588b62aa2b094b19db2a53801fd3484c1
pjh5365 <pjh5365@naver.com>
2025-01-17 15:45:09 +0900

    update

--- src/main/java/com/example/springsessionredis/auth/config/CorsConfig.java
+++ src/main/java/com/example/springsessionredis/auth/config/CorsConfig.java
@@ -10,9 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://10.0.0.6") // Vue 앱이 실행되는 도메인
-				.allowedOrigins("http://223.130.137.86") // Vue 앱이 실행되는 도메인
-				.allowedMethods("GET", "POST", "PUT", "DELETE")
-				.allowCredentials(true);
+				.allowedOrigins("http://10.0.0.6", "http://223.130.137.86")
+				.allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
